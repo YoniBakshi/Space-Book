81fc1d9bc510f7b30ef9d19bf04b8c3fc2a0ddd1
hila saadon <hilasaa6@gmail.com>
2023-01-07 13:13:45 +0200

    YALLA YALLA working

--- routes/register.js
+++ routes/register.js
@@ -4,11 +4,11 @@ const loginController = require("../controllers/register");
 
 let router = express.Router();
 
-/* /register/ => GET users listing. */
+/* /register => GET users listing. */
 router.get('/', loginController.getRegister)
 
-/* /register/data => POST users listing. */
-router.post('/data', loginController.postRegister)
+/* /register => POST users listing. */
+router.post('/', loginController.postRegister)
 
 
 module.exports = router;
