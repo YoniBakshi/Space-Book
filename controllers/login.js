d4d000eed6336546385f2695582b72bf4435409b
Yoni <Yoni@DESKTOP-E5PE3BL>
2023-01-13 16:27:51 +0200

    Validate login

--- controllers/login.js
+++ controllers/login.js
@@ -1,3 +1,4 @@
+const db = require("../models");
 exports.getLogin = (req, res, next) => {
     console.log("login get")
     //TODO validation
@@ -8,7 +9,60 @@ exports.getLogin = (req, res, next) => {
         message:req.cookies.message});
 }
 
-exports.postLogin = (req, res, next) => {
+exports.postLogin = async (req, res, next) => {
     /////////  Just started - only loads without doing anything   /////////
+    try {
+    if (!await db.User.findOne({ where: {email: req.body.EmailLogin}})) //check if the entered email is caught
+        throw new MyError(`The email is not in the system.`,`/`);
+    const validPassword = await db.User.findOne({ where: {email: req.body.EmailLogin} ,
+        attributes: ['password']})
+    if(validPassword.dataValues.password !== req.body.passwordLogin)
+        throw new MyError(`The password is not in the system.`,`/`);
+
+
+    } catch (error) {
+        if(error instanceof MyError) {
+            res.cookie("message", error.message);
+            return res.redirect(error.redirect);
+        }
+        res.status(500).send('Error occurred');
+    }
     console.log(";lkjhgfdfghjklkjhgf")
+}
+
+
+
+exports.postRegisterPassword = async (req, res, next) => {
+    try {
+        if (!req.cookies.registerData)  //check if the cookie exist
+            throw new MyError(`Registration process expired, Please start again.`,`/users/register`);
+
+        if (await db.User.findOne({ where: {email: req.cookies.registerData.userEmail}})) //check if the entered email is caught
+            throw new MyError(`The email is already in use, please choose other one.`,`/users/register`);
+
+        const { userEmail: email, userFirstName: firstName, userLastName: lastName } = req.cookies.registerData;
+        const password = req.body.passwordRegister;
+
+        if(password !== req.body.passwordConfirm) //password validation
+            throw new MyError(`Passwords do not match, please try again.`,`/users/register-password`);
+
+        await db.User.create({ email, firstName, lastName, password }); //if succeed go back to login
+        res.clearCookie("registerData");
+        res.cookie("message", "Registration successful, you can login now.");
+        return res.redirect('/');
+
+    } catch (error) {
+        if(error instanceof MyError) {
+            res.cookie("message", error.message);
+            return res.redirect(error.redirect);
+        }
+        res.status(500).send('Error occurred');
+    }
+}
+
+class MyError extends Error {
+    constructor(message, redirect) {
+        super(message);
+        this.redirect = redirect;
+    }
 }
--- controllers/register-password.js
+++ controllers/register-password.js
@@ -3,7 +3,9 @@ const db = require('../models')
 // Next was clicked - load the next pagee which is register password
 exports.getRegisterPassword = (req, res, next) => {
     //TODO validation
-    //checkCookie(req,res)
+    //to block access to register password page
+    if (!req.cookies.registerData)
+        res.redirect('/users/register')
 
     res.render('register-password', {
         titlePage: 'register-password',
@@ -17,23 +19,18 @@ exports.postRegisterPassword = async (req, res, next) => {
     try {
         if (!req.cookies.registerData)  //check if the cookie exist
             throw new MyError(`Registration process expired, Please start again.`,`/users/register`);
-        //const existingUser = await db.User.findOne({ where: { email: userEmail } });
-        const existingUser = await db.User.findOne({ where: { email: req.cookies.registerData.userEmail } });
 
-        if (existingUser) //check if the entered email is caught
+        if (await db.User.findOne({ where: {email: req.cookies.registerData.userEmail}})) //check if the entered email is caught
             throw new MyError(`The email is already in use, please choose other one.`,`/users/register`);
 
         const { userEmail: email, userFirstName: firstName, userLastName: lastName } = req.cookies.registerData;
         const password = req.body.passwordRegister;
 
-        //to debug
-/*        const users = await db.User.findAll();
-        console.log(users);*/
-
         if(password !== req.body.passwordConfirm) //password validation
             throw new MyError(`Passwords do not match, please try again.`,`/users/register-password`);
 
         await db.User.create({ email, firstName, lastName, password }); //if succeed go back to login
+        res.clearCookie("registerData");
         res.cookie("message", "Registration successful, you can login now.");
         return res.redirect('/');
 
@@ -52,3 +49,8 @@ class MyError extends Error {
         this.redirect = redirect;
     }
 }
+
+
+//to debug
+/*        const users = await db.User.findAll();
+        console.log(users);*/
