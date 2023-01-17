3d42cec4092ab4bd9444e90d911d90bdb7e3cefc
Yoni <Yoni@DESKTOP-E5PE3BL>
2023-01-17 17:36:47 +0200

    rout ex5 in home

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const MyError = require("../utils/utils");
-
+let itGoJson = []
+let index = 0;
 exports.getHome = (req, res, next) => {
 
         //TODO validation
@@ -11,9 +12,39 @@ exports.getHome = (req, res, next) => {
 }
 
 exports.postHome = async (req, res, next) => {
-    /////////  Just started - only loads without doing anything   /////////
     try {
+        // Validate request body
+        /*    if (!req.body.userName || !req.body.id || !req.body.currComment) {
+                return res.status(400).json({ message: 'All fields are required' });
+            }*/
+        /*
+            // Check if userName is a string
+            if (typeof req.body.userName !== 'string') {
+                return res.status(400).json({ message: 'userName must be a string' });
+            }
 
+            // Check if id is a valid date
+            if (!Date.parse(req.body.id)) {
+                return res.status(400).json({ message: 'id must be a valid date' });
+            }
+
+            // Check if currComment is a string
+            if (typeof req.body.currComment !== 'string') {
+                return res.status(400).json({ message: 'currComment must be a string' });
+            }*/
+
+        // If all validation checks pass, create resource
+        const name = req.session.userFullName;
+        const date = req.body.id;
+        let resource = {
+            commentField: req.body.currComment,
+            username: name,
+            id: date,
+            postId: index,
+        };
+        index++;
+        itGoJson.push(resource);
+        res.json(itGoJson);
     } catch (error) {
         if(error instanceof MyError) {
             res.cookie("message", error.message);
@@ -24,3 +55,46 @@ exports.postHome = async (req, res, next) => {
 }
 
 
+exports.getComments = async (req, res, next) => {
+
+    try {
+        const resourceId = req.params.id;
+
+        const resource = itGoJson.filter(w => w.id === resourceId);
+
+        res.status(200).json(resource)
+    } catch (error) {
+        if(error instanceof MyError) {
+            res.cookie("message", error.message);
+            return res.redirect(error.redirect);
+        }
+        res.status(500).send('Error occurred');
+    }
+}
+
+
+exports.deleteComment = async (req, res, next) => {
+
+    try {
+        const resourceId = Number(req.params.id);
+
+        // Find the index of the resource in the itGoJson array
+        const index = itGoJson.findIndex(w => w.postId === resourceId);
+
+        // If the resource is not found, return a 404 response
+        if (index === -1) {
+            return res.status(404).json({msg: `Resource not found. id ${resourceId}`});
+        }
+        // Remove the resource from the array
+        itGoJson.splice(index, 1);
+
+        // Return a success response
+        res.status(200).json({msg: "Success"});
+    } catch (error) {
+        if(error instanceof MyError) {
+            res.cookie("message", error.message);
+            return res.redirect(error.redirect);
+        }
+        res.status(500).send('Error occurred');
+    }
+}
--- routes/home.js
+++ routes/home.js
@@ -4,79 +4,11 @@ const homeController = require('../controllers/home')
 
 /* GET home page. */
 router.get('/', homeController.getHome);
-router.post('/', homeController.postHome);
-//////////////////////////////////////////////////////////////////
-let itGoJson = []
-let index = 0;
-/* GET home page. */
-/*router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});*/
-
-// Creating a resource
-//router.post('/resources', (req, res) => {
-router.post('/resources', (req, res) => {
-    // Validate request body
-/*    if (!req.body.userName || !req.body.id || !req.body.currComment) {
-        return res.status(400).json({ message: 'All fields are required' });
-    }*/
-/*
-    // Check if userName is a string
-    if (typeof req.body.userName !== 'string') {
-        return res.status(400).json({ message: 'userName must be a string' });
-    }
-
-    // Check if id is a valid date
-    if (!Date.parse(req.body.id)) {
-        return res.status(400).json({ message: 'id must be a valid date' });
-    }
-
-    // Check if currComment is a string
-    if (typeof req.body.currComment !== 'string') {
-        return res.status(400).json({ message: 'currComment must be a string' });
-    }*/
-
-    // If all validation checks pass, create resource
-    const name = req.session.userFullName;
-    const date = req.body.id;
-    let resource = {
-        commentField: req.body.currComment,
-        username: name,
-        id: date,
-        postId: index,
-    };
-    index++;
-    itGoJson.push(resource);
-    res.json(itGoJson);
-});
-
+router.post('/resources', homeController.postHome);
 
 // Getting a single resource
 // for example /resources/123 where 123 is some identifier to search for a resource
-router.delete("/del/:id", (req, res) => {
-    const resourceId = Number(req.params.id);
-
-    // Find the index of the resource in the itGoJson array
-    const index = itGoJson.findIndex(w => w.postId === resourceId);
-
-    // If the resource is not found, return a 404 response
-    if (index === -1) {
-        return res.status(404).json({msg: `Resource not found. id ${resourceId}`});
-    }
-    // Remove the resource from the array
-    itGoJson.splice(index, 1);
-
-    // Return a success response
-    res.status(200).json({msg: "Success"});
-});
-
-router.get('/resources/:id', (req, res) => {
-    const resourceId = req.params.id;
-
-    const resource = itGoJson.filter(w => w.id === resourceId);
-
-    res.status(200).json(resource)
-});
-module.exports = router;
+router.delete("/del/:id",homeController.deleteComment)
+router.get('/resources/:id', homeController.getComments )
 
 module.exports = router;
